9d0fd5c1146de4c15e76a00eec45ba2a8a4fc8dc
wst2001 <weisitong@pku.edu.cn>
2024-07-13 11:18:25 +0800

    remove ounet

--- models/networks/dualoctree_networks/graph_vae.py
+++ models/networks/dualoctree_networks/graph_vae.py
@@ -13,6 +13,7 @@ from .distributions import DiagonalGaussianDistribution
 from . import modules
 from . import dual_octree
 from . import graph_ounet
+from . import mpu
 from ocnn.nn import octree2voxel
 from ocnn.octree import Octree
 import copy
@@ -49,26 +50,67 @@ def init_weights(net, init_type='normal', gain=0.01):
     for m in net.children():
         m.apply(init_func)
 
-class GraphVAE(graph_ounet.GraphOUNet):
+class GraphVAE(torch.nn.Module):
 
-    def __init__(self, depth, channel_in, nout, full_depth=2, depth_stop = 6, depth_out=8, use_checkpoint = False,
-                resblk_type='bottleneck', bottleneck=4,resblk_num=3, code_channel=3, embed_dim=3):
-        super().__init__(depth, channel_in, nout, full_depth, depth_stop, depth_out, use_checkpoint,
-                        resblk_type, bottleneck,resblk_num)
+    def __init__(self, depth, channel_in, nout, full_depth=2, depth_stop = 6, depth_out=8, use_checkpoint = False, resblk_type='bottleneck', bottleneck=4,resblk_num=3, code_channel=3, embed_dim=3):
+        # super().__init__(depth, channel_in, nout, full_depth, depth_stop, depth_out, use_checkpoint, resblk_type, bottleneck,resblk_num)
         # this is to make the encoder and decoder symmetric
 
+        super().__init__()
+        self.depth = depth
+        self.channel_in = channel_in
+        self.nout = nout
+        self.full_depth = full_depth
+        self.depth_stop = depth_stop
+        self.depth_out = depth_out
+        self.use_checkpoint = use_checkpoint
+        self.resblk_type = resblk_type
+        self.bottleneck = bottleneck
+        self.resblk_num = resblk_num
+        self.neural_mpu = mpu.NeuralMPU(self.full_depth, self.depth_stop, self.depth_out)
+        self._setup_channels_and_resblks()
+        n_edge_type, avg_degree = 7, 7
+        self.dropout = 0.0
         n_edge_type, avg_degree = 7, 7
 
+        # encoder
+        self.conv1 = modules.GraphConv(
+            channel_in, self.channels[depth], n_edge_type, avg_degree, depth-1)
+        self.encoder = torch.nn.ModuleList(
+            [modules.GraphResBlocks(self.channels[d], self.channels[d],self.dropout,
+            self.resblk_nums[d] - 1, n_edge_type, avg_degree, d-1, self.use_checkpoint)
+            for d in range(depth, depth_stop-1, -1)])
+        self.downsample = torch.nn.ModuleList(
+            [modules.GraphDownsample(self.channels[d], self.channels[d-1])
+            for d in range(depth, depth_stop, -1)])
+
+        self.encoder_norm_out = modules.DualOctreeGroupNorm(self.channels[depth_stop])
+
+        self.nonlinearity = torch.nn.GELU()
+        
+        # decoder
+        self.decoder = torch.nn.ModuleList(
+            [modules.GraphResBlocks(self.channels[d], self.channels[d],self.dropout,
+         self.resblk_nums[d], n_edge_type, avg_degree, d-1, self.use_checkpoint)
+            for d in range(depth_stop, depth + 1)])
         self.decoder_mid = torch.nn.Module()
         self.decoder_mid.block_1 = modules.GraphResBlocks(self.channels[depth_stop], self.channels[depth_stop],self.dropout,
          self.resblk_nums[depth_stop], n_edge_type, avg_degree, depth_stop-1, self.use_checkpoint)
         self.decoder_mid.block_2 = modules.GraphResBlocks(self.channels[depth_stop], self.channels[depth_stop],self.dropout,
          self.resblk_nums[depth_stop], n_edge_type, avg_degree, depth_stop-1, self.use_checkpoint)
+        
+        self.upsample = torch.nn.ModuleList(
+            [modules.GraphUpsample(self.channels[d-1], self.channels[d])
+            for d in range(depth_stop + 1, depth + 1)])
 
-        self.decoder = torch.nn.ModuleList(
-            [modules.GraphResBlocks(self.channels[d], self.channels[d],self.dropout,
-         self.resblk_nums[d], n_edge_type, avg_degree, d-1, self.use_checkpoint)
+        # header
+        self.predict = torch.nn.ModuleList(
+            [self._make_predict_module(self.channels[d], 2)  # 这里的2就是当前节点是否要劈成八份的label
             for d in range(depth_stop, depth + 1)])
+        self.regress = torch.nn.ModuleList(
+            [self._make_predict_module(self.channels[d], 4)  # 这里的4就是王老师说的，MPU里一个node里的4个特征分别代表法向量和偏移量
+            for d in range(depth_stop, depth + 1)])
+        
 
         self.code_channel = code_channel
         ae_channel_in = self.channels[self.depth_stop]
@@ -76,6 +118,19 @@ class GraphVAE(graph_ounet.GraphOUNet):
         self.KL_conv = modules.Conv1x1(ae_channel_in, 2 * embed_dim, use_bias = True)
         self.post_KL_conv = modules.Conv1x1(embed_dim, ae_channel_in, use_bias = True)
 
+    def _setup_channels_and_resblks(self):
+        # self.resblk_num = [3] * 7 + [1] + [1] * 9
+        # self.resblk_num = [3] * 16
+        self.resblk_nums = [self.resblk_num] * 16      # resblk_num[d] 为深度d（分辨率）下resblock的数量。
+        self.channels = [4, 512, 512, 256, 128, 64, 32, 32, 24, 8]  # depth i的channel为channels[i]
+
+    def _make_predict_module(self, channel_in, channel_out=2, num_hidden=32):
+        return torch.nn.Sequential(
+        modules.Conv1x1GnGeluSequential(channel_in, num_hidden),
+        modules.Conv1x1(num_hidden, channel_out, use_bias=True))
+
+    def _get_input_feature(self, doctree):
+        return doctree.get_input_feature()
 
     def octree_encoder(self, octree, doctree): # encoder的操作是从depth到full-deth为止，在这里就是从6到2
         convs = super().octree_encoder(octree, doctree) # conv的channel随着八叉树深度从6到2的变化为[32, 64, 128, 256, 512]
@@ -139,6 +194,27 @@ class GraphVAE(graph_ounet.GraphOUNet):
             reg_voxs[d] = reg_vox_pad
 
         return logits, reg_voxs, doctree_out.octree
+
+    def create_full_octree(self, octree_in: Octree):
+        r''' Initialize a full octree for decoding.
+        '''
+
+        device = octree_in.device
+        batch_size = octree_in.batch_size
+        octree = Octree(self.depth, self.full_depth, batch_size, device)
+        for d in range(self.full_depth+1):
+            octree.octree_grow_full(depth=d)
+        return octree
+
+    def create_child_octree(self, octree_in: Octree):
+        octree_out = self.create_full_octree(octree_in)
+        octree_out.depth = self.full_depth
+        for d in range(self.full_depth, self.depth_stop):
+            label = octree_in.nempty_mask(d).long()
+            octree_out.octree_split(label, d)
+            octree_out.octree_grow(d + 1)
+            octree_out.depth += 1
+        return octree_out
 
     def forward(self, octree_in, octree_out=None, pos=None, evaluate=False): # 这里的pos的大小为[batch_size * 5000, 4]，意思是把所有batch的points都concate在一起，用4的最后一个维度来表示batch_idx
         # generate dual octrees
